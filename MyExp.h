3aaf381ab69155d0459ced5b27553ce54381696e
Woloszyn97 <73226245+Woloszyn97@users.noreply.github.com>
2021-01-06 11:50:19 +0100

    Add files via upload

--- MyExp.h
+++ MyExp.h
@@ -8,24 +8,28 @@
 class MyExp
 {
     public:
-	
+	/**metoda publiczna void MyExp()*/
         void MyExp();
-		
+		/**metoda publiczna virtual ~MyExp()*/
         virtual ~MyExp();
-		
+		/**metoda publiczna void MyExp(double x)*/
         void MyExp(double x);
-		
+		/**metoda publiczna void MyExp(const MyExp &obj)*/
         void MyExp(const MyExp &obj);
-		
+		/**metoda publiczna double value()*/
         double value();
-		
+		/**metoda publiczna void setX(double)*/
         void setX(double);
-		
+		/**metoda publiczna double getX()*/
         double getX();
 	
 
         private:
-	
+	/**Funkcja value ma zwracać wartość \f$e^x\f$
+
+, która jest wyliczona na podstawie pierwszych 10 wyrazów szeregu
+* /*\f$e^x=\sum_{k=0}^\infty=\frac{x^k}{k!}\f$ \image html 1.png
+*/
             double mX();
 };
 
